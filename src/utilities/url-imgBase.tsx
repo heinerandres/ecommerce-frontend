a531a9ffc76c2882a3457203cb12f0ef943bdd78
heinerandres <heiner.andres@outlook.com>
2026-08-08 19:50:31 -0600

    Update base URL for image uploads

--- src/utilities/url-imgBase.tsx
+++ src/utilities/url-imgBase.tsx
@@ -1,3 +1,3 @@
-export const _base = "https://ecommerce-back-gbe4fndmdxfdctab.canadacentral-01.azurewebsites.net/uploads/";
+export const _base = "https://ecommerce-backend-3c9u.onrender.com/uploads/";
 
 /* export const _base = "http://localhost:4000/uploads/"; */
